fdcc04e1d44890768b8fd00f50e49c172e2e98dd
Brice Maltby <brice.maltby@gmail.com>
2019-02-26 23:27:07 -0600

    package => use-package

--- init.el
+++ init.el
@@ -1,58 +1,103 @@
-(require 'package)
-
-; List the packages you want
-(setq package-list '(evil
-                     evil-leader
-		     helm
-		     projectile))
-
-; Add Melpa as the default Emacs Package repository
-; only contains a very limited number of packages
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
-; Activate all the packages (in particular autoloads)
+;; Use-package setup
 (package-initialize)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-; Update your local package index
-(unless package-archive-contents
-  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-; Install all missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
+(eval-when-compile
+  (require 'use-package))
 
-(require 'evil)
-(evil-mode t)
+(setq use-package-always-ensure t)
 
-(require 'evil-leader)
-(global-evil-leader-mode)
-(evil-leader/set-leader ",")
-(evil-leader/set-key
-  "b" 'switch-to-buffer
-  "w" 'save-buffer)
-(custom-set-variables
- '(package-selected-packages (quote (projectile evil-leader evil))))
-(custom-set-faces
- )
+;; Packages
+(use-package projectile
+	     :config
+	     (projectile-mode)
+	     (setq projectile-require-root-project nil))
 
-(require 'helm)
-(helm-mode 1)
-(setq-default helm-M-x-fuzzy-match t)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(use-package helm
+	     :config
+	     (recentf-mode 1)
+	     (setq helm-mode-fuzzy-match t)
+	     (setq helm-completion-in-region-fuzzy-match t)
+	     (setq helm-candidate-number-limit 100)
+	     (setq helm-M-x-fuzzy-match t)
+	     :bind (("M-x" . helm-M-x)
+		    ("C-x C-f" . helm-find-files)))
 
-(require 'projectile)
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(use-package helm-projectile
+	     :config
+	     (helm-projectile-on)
+	     :bind (("C-c p f" . helm-projectile-find-file)
+		    ("C-c p s" . helm-projectile-ag)))
 
-; Visuals
-(add-to-list 'default-frame-alist '(font . "menlo-10"))
+(use-package exec-path-from-shell
+	     :config (exec-path-from-shell-initialize))
 
-; Motion
+(use-package evil
+	     :config
+	     (evil-mode))
+
+(use-package helm-ag)
+
+(use-package magit)
+
+(use-package tide)
+
+;; UI / UX
+(load-theme 'grandshell t)
+(set-face-attribute 'default nil :height 110)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(show-paren-mode t)
+(global-hl-line-mode +1)
+(set-face-background 'hl-line "#4A4A4A")
+(setq evil-insert-state-cursor '(box "#00CE7E")
+      evil-normal-state-cursor '(box "#F7FF57")
+      evil-visual-state-cursor '(box "red")
+      evil-replace-state-cursor '(box "blue")
+      )
+(setq fill-column 80)
+(setq visible-bell nil)
+(setq indent-tabs-mode nil)
+(setq mouse-autoselect-window t)
+(setq mouse-drag-copy-region nil)
+(global-unset-key [mouse-2])
+(setq mouse-wheel-follow-mouse t)
+(setq mouse-wheel-progressive-speed nil)
+(setq mouse-wheel-scroll-amount '(2 ((shift) . 1)))
+(setq js-indent-level 2)
+
+;; Disable graphical dialog boxes
+(defadvice yes-or-no-p (around prevent-dialog activate)
+  "Prevent 'yes-or-no-p' from activating a dialog."
+  (let ((use-dialog-box nil))
+    ad-do-it))
+(defadvice y-or-n-p (around prevent-dialog-yorn activate)
+  "Prevent 'y-or-n-p' from activating a dialog."
+  (let ((use-dialog-box nil))
+    ad-do-it))
+
+;; Motion
 (global-set-key (kbd "C-c h")  'windmove-left)
 (global-set-key (kbd "C-c l") 'windmove-right)
-(global-set-key (kbd "C-c j")    'windmove-up)
-(global-set-key (kbd "C-c k")  'windmove-down)
+(global-set-key (kbd "C-c k")    'windmove-up)
+(global-set-key (kbd "C-c j")  'windmove-down)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (tide use-package magit helm-projectile helm-ag grandshell-theme exec-path-from-shell evil-leader))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
