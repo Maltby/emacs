2db4d22630f1761a1bd6161f1229397493c0dfe5
Brice Maltby <b@bricemaltby.com>
2021-07-25 09:44:56 -0500

    Install packages if missing

--- init.el
+++ init.el
@@ -1,14 +1,19 @@
-(require 'package)
-
-;; (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-
 ;; Use-package setup
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(setq package-archives '(
+        ("melpa" . "http://melpa.org/packages/")
+))
+
+;; Manual package-initialize
+(require 'package)
 (package-initialize)
-;; don't load packages with 'package ?
-(setq package-enable-at-startup nil)
-(require 'use-package)
-(require 'org)
+
+;; Install if missing
+(unless (package-installed-p 'use-package)
+	(package-install 'use-package))
+(unless (package-installed-p 'org)
+	(package-install 'org))
+
+;; Load main.org
 (org-babel-load-file
  (expand-file-name "main.org"
 		   user-emacs-directory))
